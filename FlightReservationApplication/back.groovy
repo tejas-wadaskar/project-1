6c827bcf4863b4f69ab8abbb9e0b2535a98b84ed
tejas wadaskar <tejaswadaskar@tejass-MacBook-Air.local>
2026-04-22 19:02:19 +0530

    v1

---
+++ FlightReservationApplication/back.groovy
@@ -0,0 +1,18 @@
+pipeline 
+    agent any
+    stages {
+        stage("pull") {
+            sh // 
+        
+        }
+
+        stage("build") {
+            sh // 
+        }
+        stage("QA") {
+            sh // 
+        }
+        stage("docker") {
+            sh //
+        }
+} 
